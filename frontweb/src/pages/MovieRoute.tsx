dae170c5a959e0d2cf0cfce3aa9b2984dbfbc8c8
moduretick <robertoreboucasdemelo@gmail.com>
2021-12-06 09:17:18 -0300

    Add MovieRoute

---
+++ frontweb/src/pages/MovieRoute.tsx
@@ -0,0 +1,15 @@
+import Movie from 'pages/Movie';
+import MovieDetails from 'pages/MovieDetails';
+import { Switch, Route } from 'react-router-dom';
+
+const MovieRoute = () => (
+  <Switch>
+    <Route path="/movies" exact>
+      <Movie />
+    </Route>
+    <Route path="/movies/:movieId">
+      <MovieDetails />
+    </Route>
+  </Switch>
+);
+export default MovieRoute;
